6605973ef92d6f44ed3d4f29474dbaddd3bcf964
Evgeny <zobnin@gmail.com>
2019-01-10 21:05:33 +0300

    add crash reporting lib

--- app/src/main/java/ru/execbit/aiosmscallslog/App.kt
+++ app/src/main/java/ru/execbit/aiosmscallslog/App.kt
@@ -1,6 +1,7 @@
 package ru.execbit.aiosmscallslog
 
 import android.app.Application
+import com.devs.acr.AutoErrorReporter
 import com.mohamadamin.kpreferences.base.KPreferenceManager
 
 class App: Application() {
@@ -12,5 +13,10 @@ class App: Application() {
         super.onCreate()
         PACKAGE_NAME = packageName
         KPreferenceManager.initialize(this, name = "${packageName}_preferences")
+
+        AutoErrorReporter.get(this)
+          .setEmailAddresses("zobnin@gmail.com")
+          .setEmailSubject("AIO SMS & Calls crash report")
+          .start()
     }
 }
